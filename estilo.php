1bf885c114f27c35a24b725eea69a3a48c7dbcb8
Alvin Gil Saldaña <alvin_gil@hotmail.com>
2023-09-24 15:35:54 -0500

    Add files via upload

---
+++ estilo.php
@@ -0,0 +1,41 @@
+<?php header("Content-type: text/css; charset:UTF-8");
+$color_principal = "#141E46" ;
+$color_secundario = "#C70039" ;
+$color_opcional = "#FFF5E0" ;
+?>
+
+.btn{
+  background:#FF6969 ;
+  color: #141E46 ;
+  width: 80px;
+  height: 50px;
+  font-size: 20px;
+  border-color: #C70039;
+}
+body{
+  background:<?php echo $color_principal ; ?>;
+}
+.saludo{
+  width:190px;
+  height:40px;
+}
+.app{
+  color:#FF6969;
+  font-size:60px;
+}
+.parrafo{
+  font-size: 20px;
+  color:<?php echo $color_opcional; ?>;
+}
+.lbl{
+  color:#FFF5E0;
+  font-size: 30px;
+}
+.btnp{
+  background:#FF6969 ;
+  color: #141E46 ;
+  width: 80px;
+  height: 30px;
+  font-size: 20px;
+  border-color: #C70039;
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>estilos dinamicos con php</title>
+    <link rel="stylesheet"type="text/css" href="estilo.php">
+  </head>
+  <body>
+  <center>  <h1 class="app">app para estilos dinamicos</h1></center>
+  <center>
+    <form class="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <input class="saludo" type="text" name="saludo" value="">
+      <input class="btn" type="submit" name="enviar" value="enviar">
+    </form>
+    </center>
+
+  <br>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $saludo = $_REQUEST['saludo'];
+    ?>
+    <center>
+      <p class="parrafo"><?php echo$saludo ; ?></p>
+    </center>
+  <?php  }?>
+    <br>
+      <center><a href="procesar.php"><p class="parrafo">panel</p></a></center>
+
+
+    </body>
+</html>
